127efbf8e1c89cf255ddfdfd5bc1dc8025b49d01
administration <administration@goiteens.ua>
2026-04-06 12:56:46 +0300

    allow to FINANCE to export google sheet

--- src/helpers/featureAccess.js
+++ src/helpers/featureAccess.js
@@ -1,18 +1,17 @@
 import { UserRole } from './enums';
 
-const GOOGLE_SHEETS_ANALYTICS_FINANCE_ALLOWLIST = [10];
 const GOOGLE_SHEETS_ANALYTICS_DENIED_ROLES = [
   UserRole.CEO,
   UserRole.ACCOUNTANT,
 ];
 
-export const canAccessGoogleSheetsAnalytics = ({ userId, userRole }) => {
+export const canAccessGoogleSheetsAnalytics = ({ userRole }) => {
   if (GOOGLE_SHEETS_ANALYTICS_DENIED_ROLES.includes(userRole)) {
     return false;
   }
 
   if (userRole === UserRole.FINANCE) {
-    return GOOGLE_SHEETS_ANALYTICS_FINANCE_ALLOWLIST.includes(Number(userId));
+    return true;
   }
 
   return true;
